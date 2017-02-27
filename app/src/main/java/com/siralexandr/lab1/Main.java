3e1651655dd16f1bfbe2566958b9967929070096
futuris2012 <sania.ratushnyy@gmail.com>
2017-02-27 21:54:17 +0400

    Initial commit

---
+++ app/src/main/java/com/siralexandr/lab1/Main.java
@@ -0,0 +1,19 @@
+package com.siralexandr.lab1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClick(View v){
+        TextView tv=(TextView)findViewById(R.id.textView);
+        tv.setText("hello");
+    }
+}
